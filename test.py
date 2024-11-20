4b90fdc520c02ac83cefe7479ae780b501cde904
mohammadReza1993 <39256013+mohammadReza1993@users.noreply.github.com>
2024-11-20 16:07:10 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 import numpy as np
 from PIL import Image
-model = YOLO('/data/mreza/CaveSegmentation/code/Yolo/runs/segment/train9/weights/best.pt')
+model = YOLO('best.pt')
 image_address = '/data/mreza/CaveSegmentation/data/images/val/00614.jpg'
 results = model.predict(image_address, imgsz=[640,960], conf=0.4)
 # masks = results.xyxy[0][:, 4].numpy()

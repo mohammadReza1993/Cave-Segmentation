c6b185d55d0bec60525c1a171403922b464fa143
mohammadReza1993 <39256013+mohammadReza1993@users.noreply.github.com>
2024-11-20 14:51:18 -0500

    Update train.py

--- train.py
+++ train.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8n-seg.pt')  # load a pretrained model (recommended for training)
 
-model.train(data='/data/mreza/CaveSegmentation/code/Yolo/config.yaml', epochs=300, imgsz=640, batch = 16, device = 1)
+model.train(data='config.yaml', epochs=300, imgsz=640, batch = 16, device = 1)
